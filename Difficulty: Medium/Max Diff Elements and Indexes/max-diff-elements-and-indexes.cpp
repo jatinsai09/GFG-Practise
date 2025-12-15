4a6d03fac5b9cc26a167295a5b540cad54ee822e
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-15 22:57:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Diff Elements and Indexes/max-diff-elements-and-indexes.cpp
@@ -0,0 +1,20 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxValue(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int mx1 = INT_MIN, mn1 = INT_MAX;
+        int mx2 = INT_MIN, mn2 = INT_MAX;
+        
+        for (int i = 0; i < n; i++) {
+            mx1 = max(mx1, arr[i] - i);
+            mx2 = max(mx2, arr[i] + i);
+            mn1 = min(mn1, arr[i] - i);
+            mn2 = min(mn2, arr[i] + i);
+        }
+        
+        return max(mx1 - mn1, mx2 - mn2);
+    }
+};
