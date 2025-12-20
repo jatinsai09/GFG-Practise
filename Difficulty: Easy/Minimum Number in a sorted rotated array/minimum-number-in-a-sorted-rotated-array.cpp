14980b9f93f5e918177af6847bafd969c6fe7970
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-20 10:33:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Number in a sorted rotated array/minimum-number-in-a-sorted-rotated-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    // Function to find the minimum element in sorted and rotated array.
+    int minNumber(int arr[], int low, int high) {
+        // Your code here
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return arr[low];
+    }
+};
