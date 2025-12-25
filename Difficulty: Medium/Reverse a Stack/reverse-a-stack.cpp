78436566794fc3bd39071cc012cd37a7c40869e6
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-25 18:10:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.cpp
@@ -1,14 +1,29 @@
 class Solution {
   public:
-    void reverseStack(stack<int> &st) {
-        // code here
-        stack<int> t;
-        
-        while (!st.empty()) {
-            t.push(st.top());
-            st.pop();
+    void insertAtBottom(stack<int>& st, int x) {
+        if (st.empty()) {
+            st.push(x);
+            return;
         }
         
-        st = t;
+        int top = st.top();
+        st.pop();
+        
+        insertAtBottom(st, x);
+        
+        st.push(top);
+    }
+    void reverseStack(stack<int> &st) {
+        // code here
+        if (st.empty()) {
+            return;
+        }
+        
+        int top = st.top();
+        st.pop();
+        
+        reverseStack(st);
+        
+        insertAtBottom(st, top);
     }
 };
