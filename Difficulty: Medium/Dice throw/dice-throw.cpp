b1d4aef387f382c0d1eda91d226ea3b4688a53a1
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-07 08:05:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dice throw/dice-throw.cpp
+++ Difficulty: Medium/Dice throw/dice-throw.cpp
@@ -5,15 +5,11 @@ class Solution {
         vector<long long> dp(x + 1, 0);
         dp[0] = 1;
 
-        for (int i = 1; i <= n; i++)
-        {
+        for (int i = 1; i <= n; i++) {
             vector<long long> ndp(x + 1, 0);
-            for (int s = 1; s <= x; s++)
-            {
-                for (int f = 1; f <= m; f++)
-                {
-                    if (s - f >= 0)
-                    {
+            for (int s = 1; s <= x; s++) {
+                for (int f = 1; f <= m; f++) {
+                    if (s - f >= 0) {
                         ndp[s] += dp[s - f];
                     }
                 }
