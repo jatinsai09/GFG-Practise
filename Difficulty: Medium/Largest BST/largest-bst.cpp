fae9dec1b66250f9ae3f43a85b4aa1337e3cf088
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-06-05 22:15:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.cpp
+++ Difficulty: Medium/Largest BST/largest-bst.cpp
@@ -14,7 +14,6 @@ struct Node {
 class Solution {
 public:
     struct Info {
-        bool isBST;
         int size;
         int mn;
         int mx;
@@ -23,24 +22,22 @@ public:
     int ans = 0;
     Info dfs(Node* root) {
         if (!root) {
-            return {true, 0, INT_MAX, INT_MIN};
+            return {0, INT_MAX, INT_MIN};
         }
 
         Info L = dfs(root->left);
         Info R = dfs(root->right);
 
         Info cur;
-        if (L.isBST && R.isBST && L.mx < root->data && R.mn > root->data) {
-            cur.isBST = true;
+        if (L.mx < root->data && R.mn > root->data) {
             cur.size = L.size + R.size + 1;
             cur.mn = min(L.mn, root->data);
             cur.mx = max(R.mx, root->data);
             ans = max(ans, cur.size);
         } else {
-            cur.isBST = false;
-            //cur.size = 0;
-            //cur.mn = INT_MIN;
-            //cur.mx = INT_MAX;
+            cur.size = 0;
+            cur.mn = INT_MIN;
+            cur.mx = INT_MAX;
         }
 
         return cur;
