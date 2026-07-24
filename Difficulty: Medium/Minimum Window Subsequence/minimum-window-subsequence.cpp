498bf1c0d340ad407b5168775c53e05ef90cc225
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-24 18:25:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Window Subsequence/minimum-window-subsequence.cpp
+++ Difficulty: Medium/Minimum Window Subsequence/minimum-window-subsequence.cpp
@@ -2,26 +2,22 @@ class Solution {
   public:
     string minWindow(string& s1, string& s2) {
         // Code here
-        int n = s1.size();
-        int m = s2.size();
-        int bestLen = 1e9;
-        int bestL = -1;
+        int n = s1.size(), m = s2.size();
+        int bestLen = 1e9, bestL = -1;
 
         for (int i = 0; i < n; i++) {
-            int j = 0;
-            int k = i;
-
+            if (i <= bestL) {
+                continue;
+            }
+            
+            int j = 0, k = i;
             while (k < n && j < m) {
-                if (s1[k] == s2[j]) {
-                    j++;
-                }
+                j += (s1[k] == s2[j]);
                 k++;
             }
 
             if (j == m) {
-                int end = k - 1;
-                j = m - 1;
-                int start = end;
+                int end = k - 1, start = end, j = m - 1;
 
                 while (start >= i) {
                     if (s1[start] == s2[j]) {
