3fb37018da723d45f96889014df64500a0bd94de
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-15 22:56:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Numbers Without d as Digit/numbers-without-d-as-digit.cpp
+++ Difficulty: Hard/Numbers Without d as Digit/numbers-without-d-as-digit.cpp
@@ -9,7 +9,7 @@ class Solution {
         string s = to_string(n);
         long long len = s.size(), res = 0, has_d = 0;
         
-        vector<long long> p9(11);
+        vector<long long> p9(10);
         p9[0] = 1;
         for (int i = 1; i <= 10; i++) {
             p9[i] = p9[i - 1] * 9; 
@@ -18,11 +18,8 @@ class Solution {
         for (int i = 0; i < len; i++) {
             int dg = s[i] - '0', op = 0;
             
-            int start = (i == 0 ? 1 : 0);
-            for (int j = start; j < dg; j++) {
-                if (j != d) {
-                    op++;
-                }
+            for (int j = (i == 0); j < dg; j++) {
+                op += (j != d);
             }
             
             res += op * p9[len - (i + 1)];
@@ -34,12 +31,7 @@ class Solution {
         }
         
         for (int i = 1; i < len; i++) {
-            int op = 0;
-            for (int j = 1; j <= 9; j++) {
-                if (j != d) {
-                    op++;
-                }
-            }
+            int op = 9 - (d > 0);
             
             res += op * p9[i - 1];
         }
