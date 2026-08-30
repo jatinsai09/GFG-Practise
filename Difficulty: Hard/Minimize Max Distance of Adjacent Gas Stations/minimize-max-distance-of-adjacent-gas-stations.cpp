090f61e1da59711c752c467bf2d6eea8e1a140df
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-30 19:36:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance of Adjacent Gas Stations/minimize-max-distance-of-adjacent-gas-stations.cpp
+++ Difficulty: Hard/Minimize Max Distance of Adjacent Gas Stations/minimize-max-distance-of-adjacent-gas-stations.cpp
@@ -4,24 +4,17 @@ class Solution {
         // Code here
         int n = stations.size();
         
-        double l = 0, r = 0, m;
-        for (int i = 1; i < n; i++) {
-            double dist = stations[i] - stations[i - 1];
-            
-            r = max(r, dist);
-        }
-        
         auto can = [&](double m) {
             int need = 0;
             for (int i = 1; i < n; i++) {
                 int dist = stations[i] - stations[i - 1];
                 
                 int req = dist / m;
-                
-                if (dist == m * req) {
-                    req--;
+                if (req * m < dist) {
+                    req++;
                 }
-                need += req;
+                
+                need += req - 1;
                 
                 if (need > k) {
                     return false;
@@ -31,7 +24,7 @@ class Solution {
             return true;
         };
         
-        double df = 1e-6;
+        double l = 0, r = stations[n - 1] - stations[0], m;
         while (r - l > 1e-6){
             m = (l + r) / 2.0;
             
