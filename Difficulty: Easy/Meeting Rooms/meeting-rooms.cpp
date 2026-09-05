2d8b51be06815d4401fe5f7c471b3c4fd3705365
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-09-05 18:43:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
@@ -6,8 +6,8 @@ class Solution {
         int n = arr.size();
         sort(begin(arr), end(arr));
         
-        for(int i = 1;i < n; i++){
-            if(arr[i - 1][1] > arr[i][0]){
+        for(int i = 1; i < n; i++){
+            if (arr[i - 1][1] > arr[i][0]){
                 return false;
             }
         }
